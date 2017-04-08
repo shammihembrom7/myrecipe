99f578af1f73575e958b03a8e4e155e5cd53ef1b
sam <shammihembrom7@gmail.com>
2017-04-08 17:15:32 +0530

    added registration, login, logout

--- recepies/forms.py
+++ recepies/forms.py
@@ -1,5 +1,12 @@
 from django import forms
+from django.contrib.auth import (
+    authenticate,
+    get_user_model,
+    login,
+    logout,
+)
 
+User = get_user_model()
 from .models import Recipe
 
 class PostForm(forms.ModelForm):
@@ -9,3 +16,32 @@ class PostForm(forms.ModelForm):
             "title",
             "recipe",
          ]
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self, *args, **kwargs):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+
+            if not user:
+                raise forms.ValidationError("This user does not exit")
+            if not user.check_password(password):
+                raise forms.ValidationError("Incorrect password!")
+        return super(UserLoginForm, self).clean(*args,**kwargs)
+
+class UserRegForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password'
+        ]
+    def clean(self, *args, **kwargs):
+        return super(UserRegForm,self).clean(*args, **kwargs)
--- recepies/views.py
+++ recepies/views.py
@@ -1,10 +1,13 @@
 # Create your views here.
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
 from .models import Recipe
 from .forms import PostForm
 
+@login_required(login_url='/login/')
 def recipe_create(request):
     form = PostForm(request.POST or None)
     if form.is_valid():
@@ -22,18 +25,16 @@ def recipe_detail(request, id=None):
     }
     return render(request, "detail.html", content)
 
+@login_required(login_url='/login/')
 def recipe_list(request):
-    # if request.user.is_authenticated():
-    #     content = {
-    #         "title": "My Recipe List"
-    #         }
-    # else:
-    #     content = {
-    #         "title": "List"
-    #     }
+    if request.user.is_authenticated():
+        u = request.user.username
+    else:
+        u = "login please!"
     queryset = Recipe.objects.all()
     content = {
-        "objList": queryset
+        "objList": queryset,
+        "user": u
     }
 
     return render(request, "index.html", content)
@@ -44,3 +45,61 @@ def recipe_update(request):
 
 def recipe_delete(request):
     return HttpResponse("<h1>delete</h1>")
+
+#===================================================================
+
+from django.contrib.auth import (
+    authenticate,
+    get_user_model,
+    login,
+    logout,
+)
+from django.contrib.auth.models import User
+from .forms import UserLoginForm, UserRegForm
+
+
+def login_view(request):
+    if request.user.is_authenticated():
+        return redirect('/')
+    title = "login"
+    next = request.GET.get('next')
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect('/')
+
+    return render(request, "form.html",{'form':form, 'title':title})
+
+def register_view(request):
+    title = "register"
+    next = request.GET.get('next')
+    form = UserRegForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        email = form.cleaned_data.get('email')
+
+        user = User.objects.create_user(username, email, password)
+        user.save()
+
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect("/")
+
+    context = {
+        "form": form,
+        "title":title
+    }
+    return render(request, "form.html", context)
+
+def logout_view(request):
+    logout(request)
+    next = request.GET.get('next')
+    if next:
+        return redirect(next)
+    return redirect('/')
